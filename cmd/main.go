2dd002c82c07c6fa0ceeecb121867fcfca19e682
Golam Mostafa Shuvo <gmshuvo@deepmindlabs.ai>
2024-03-13 11:16:40 +0600

    fixed: http on 127.0.0.11:53: server misbehaving

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"os"
 	"github.com/gin-gonic/gin"
 	c "github.com/gmshuvo/go-gin-postgres/config"
 	repo "github.com/gmshuvo/go-gin-postgres/repositories"
 	r "github.com/gmshuvo/go-gin-postgres/routes"
 	u "github.com/gmshuvo/go-gin-postgres/utils"
-	
+	"os"
 )
 
 func main() {
@@ -17,9 +16,17 @@ func main() {
 	router := gin.Default()
 	// make group for  routes
 	Routes := router.Group("/api/v1")
-	r.UserRouters(Routes)
-
+	router.Use(gin.Logger())
+	router.SetTrustedProxies([]string{"192.168.0.1"})
+	router.Use(gin.Recovery())
 	// Migrate the schema
 	repo.MigrateDB()
-	router.Run(":" + os.Getenv("PORT"))
+
+	r.UserRouters(Routes)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default to port 8080 if not specified
+	}
+	router.Run(":" + port)
 }
